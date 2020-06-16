3e89c187718566c08ef572098ecacedcdcad609c
ss-koshala <koshala.manojeewa@silverstripers.com>
2020-06-16 05:52:05 +0530

    bug fixing

--- _config.php
+++ _config.php
@@ -2,7 +2,7 @@
 use SilverStripe\Core\Config\Config;
 use SilverStripe\Admin\LeftAndMain;
 
-Config::inst()->merge(
+Config::modify()->set(
     LeftAndMain::class,
     'extra_requirements_css',
     array(basename(dirname(__FILE__)) . '/css/GridFieldCopyButton.css')
